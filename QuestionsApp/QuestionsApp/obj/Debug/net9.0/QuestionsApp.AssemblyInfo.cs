03f0e3941a32fab218bfe2658d06c83eed9080c8
BloodBoyce <davidkurz70@gmail.com>
2025-03-06 14:58:12 +0100

    Nugets Added

--- QuestionsApp/QuestionsApp/obj/Debug/net9.0/QuestionsApp.AssemblyInfo.cs
+++ QuestionsApp/QuestionsApp/obj/Debug/net9.0/QuestionsApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuestionsApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03beaf7f8cd02aebffc2c5480e45c71a5d2a76e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4bb29dd61d0ce0da63362137d73e7469c8fa1d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuestionsApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuestionsApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
